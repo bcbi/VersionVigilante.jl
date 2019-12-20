98fd75301b28268acabc09e1c33b9f7dfa7d4a46
Dilum Aluthge <dilum@aluthge.com>
2019-12-20 10:59:38 -0500

    Update compare_versions.jl

--- src/compare_versions.jl
+++ src/compare_versions.jl
@@ -15,18 +15,18 @@ function compare_versions(master_version::VersionNumber,
                           gh_set_output::Bool = get(ENV, "GITHUB_ACTIONS", "") == "true",
                           gh_set_output_io::IO = stdout)::Nothing
     if head_version > master_version
-        _gh_set_output_println(gh_set_output, gh_set_output_io, "good_or_bad", "good")
+        _gh_set_output_println(gh_set_output, gh_set_output_io, "compare_versions", "success")
         @info("Version number has increased")
     elseif head_version == master_version
         if isprerelease(head_version) && isprerelease(master_version) && allow_unchanged_prerelease
-            _gh_set_output_println(gh_set_output, gh_set_output_io, "good_or_bad", "good")
+            _gh_set_output_println(gh_set_output, gh_set_output_io, "compare_versions", "success")
             @info("Version number did not change, but it is a prerelease so this is allowed")
         else
-            _gh_set_output_println(gh_set_output, gh_set_output_io, "good_or_bad", "bad")
+            _gh_set_output_println(gh_set_output, gh_set_output_io, "compare_versions", "failure")
             throw(ErrorException("Version number is unchanged, which is not allowed"))
         end
     else
-        _gh_set_output_println(gh_set_output, gh_set_output_io, "good_or_bad", "bad")
+        _gh_set_output_println(gh_set_output, gh_set_output_io, "compare_versions", "failure")
         throw(ErrorException("Version number decreased, which is not allowed"))
     end
     return nothing
