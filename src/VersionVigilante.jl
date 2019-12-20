b844a317cd7bbe31dc0795e204f0523cf05fae96
Dilum Aluthge <dilum@aluthge.com>
2019-12-20 11:46:17 -0500

    Remove outdated labels; print master and head versions to output (#42)

--- src/VersionVigilante.jl
+++ src/VersionVigilante.jl
@@ -5,6 +5,7 @@ import Pkg
 include("compare_versions.jl")
 include("main.jl")
 include("parse_project.jl")
+include("set_output.jl")
 include("utils.jl")
 
 end # module
--- src/compare_versions.jl
+++ src/compare_versions.jl
@@ -11,16 +11,22 @@ end
 
 function compare_versions(master_version::VersionNumber,
                           head_version::VersionNumber;
-                          allow_unchanged_prerelease::Bool)::Nothing
+                          allow_unchanged_prerelease::Bool, 
+                          gh_set_output::Bool = get(ENV, "GITHUB_ACTIONS", "") == "true",
+                          gh_set_output_io::IO = stdout)::Nothing
     if head_version > master_version
+        _gh_set_output_println(gh_set_output, gh_set_output_io, "compare_versions", "success")
         @info("Version number has increased")
     elseif head_version == master_version
         if isprerelease(head_version) && isprerelease(master_version) && allow_unchanged_prerelease
+            _gh_set_output_println(gh_set_output, gh_set_output_io, "compare_versions", "success")
             @info("Version number did not change, but it is a prerelease so this is allowed")
         else
+            _gh_set_output_println(gh_set_output, gh_set_output_io, "compare_versions", "failure")
             throw(ErrorException("Version number is unchanged, which is not allowed"))
         end
     else
+        _gh_set_output_println(gh_set_output, gh_set_output_io, "compare_versions", "failure")
         throw(ErrorException("Version number decreased, which is not allowed"))
     end
     return nothing
